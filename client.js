a2c6b28b8edbd262cc9328a85132174f3d5a30a8
Cristian9407 <60494428+Cristian9407@users.noreply.github.com>
2025-10-16 10:50:55 -0500

    Update client.js

--- client.js
+++ client.js
@@ -47,7 +47,7 @@ const connect = async () => {
       }, {
          // This is the Baileys connection options section
          version: [2, 3000, 1028302242], // To see the latest version : https://wppconnect.io/whatsapp-versions/
-         browser: ['MacOS', 'Safari', '14.4.1'],
+         browser: ['Ubuntu', 'Firefox', '20.0.00'],
          shouldIgnoreJid: jid => {
             return /(newsletter|bot)/.test(jid)
          }
