253a29e435c509a7da986fb5143baf43f1aebd42
Cristian9407 <60494428+Cristian9407@users.noreply.github.com>
2025-11-21 18:53:53 -0500

    Update dado.js

--- plugins/dado.js
+++ plugins/dado.js
@@ -1,47 +1,31 @@
-exports.run = {
-   async: async (m, {
-      client,
-      body,
-      users,
-      groupSet,
-      setting,
-      isAdmin,
-      isBotAdmin,
-      Func
-   }) => {
-      try {
-         if (groupSet.filter && !isAdmin && isBotAdmin && !m.fromMe) {
-            let toxic = setting.toxic
-            if (body && (new RegExp('\\b' + toxic.join('\\b|\\b') + '\\b')).test(body.toLowerCase())) {
-               groupSet.member[m.sender].warning += 1
-               let warning = groupSet.member[m.sender].warning
-               if (warning > 2) return client.reply(m.chat, Func.texted('bold', `🚩 Advertencia : [ 3 / 3 ], good bye ~~`), m).then(() => {
-                  client.groupParticipantsUpdate(m.chat, [m.sender], 'remove').then(async () => {
-                     groupSet.member[m.sender].warning = 0
-                     client.sendMessage(m.chat, {
-                        delete: {
-                           remoteJid: m.chat,
-                           fromMe: isBotAdmin ? false : true,
-                           id: m.key.id,
-                           participant: m.sender
-                        }
-                     })
-                  })
-               })
-               return client.reply(m.chat, `乂  *W A R N I N G* \n\nRecibiste una advertencia : [ ${warning} / 3 ]\nSi recibes 3 advertencias serás expulsado automáticamente del grupo.`, m).then(() => client.sendMessage(m.chat, {
-                  delete: {
-                     remoteJid: m.chat,
-                     fromMe: isBotAdmin ? false : true,
-                     id: m.key.id,
-                     participant: m.sender
-                  }
-               }))
-            }
-         }
-      } catch (e) {
-         return client.reply(m.chat, Func.jsonFormat(e), m)
-      }
-   },
-   group: true,
-   exception: true
+
+let handler = async (m, { conn }) => {
+let stickdados = [
+  'https://tinyurl.com/gdd01',
+  'https://tinyurl.com/gdd02',
+  'https://tinyurl.com/gdd003',
+  'https://tinyurl.com/gdd004',
+  'https://tinyurl.com/gdd05',
+  'https://tinyurl.com/gdd006'
+]
+let url = stickdados[Math.floor(Math.random() * stickdados.length)]
+conn.sendFile(m.chat, url, 'error.webp', null, m, { asSticker: true })}
+handler.command = ['dado', 'dados', 'dadu'] 
+export default handler;
+
+/* const handler = async (m, {conn}) => {
+ 
+  conn.sendFile(m.chat, `https://api.lolhuman.xyz/api/sticker/dadu?apikey=${lolkeysapi}`, 'error.webp', '', m, {asSticker: true});
+};
+handler.command = ['dado', 'dados', 'dadu'];
+export default handler;*/
+
+/* import { sticker } from '../src/libraries/sticker.js'
+let handler = async (m, { conn }) => {
+const dir = ['https://www.random.org/dice/dice1.png', 'https://www.random.org/dice/dice2.png', 'https://www.random.org/dice/dice3.png', 'https://www.random.org/dice/dice4.png', 'https://www.random.org/dice/dice5.png', 'https://www.random.org/dice/dice6.png'];
+let img = dir[Math.floor(Math.random() * dir.length)]
+//let stiker = await sticker(img, false, global.packname, global.author)
+await conn.sendFile(m.chat, img, 'error.webp', '', m, { asSticker: true })
 }
+handler.command = ['dado', 'dados', 'dadu']
+export default handler*/
