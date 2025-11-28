337d89a2de9722af5c5ca6132e110dffbd3e306a
Cristian9407 <60494428+Cristian9407@users.noreply.github.com>
2025-11-28 00:05:58 -0500

    Update ji.js

--- plugins/utilities/ji.js
+++ plugins/utilities/ji.js
@@ -7,7 +7,7 @@ export const run = {
       isPrefix,
       Utils
    }) => {
-      client.sendFile(m.chat, './lib/media/image/empire.webp', '', m)
+      client.sendFile(m.chat, './lib/media/image/empire.webp', 'error.webp', m)
    },
    error: false
 }
