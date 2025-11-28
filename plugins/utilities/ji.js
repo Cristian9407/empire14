5a619956b68a79cb19b6653a78e7abc97cd2e3b5
Cristian9407 <60494428+Cristian9407@users.noreply.github.com>
2025-11-28 00:07:59 -0500

    Update ji.js

--- plugins/utilities/ji.js
+++ plugins/utilities/ji.js
@@ -7,7 +7,7 @@ export const run = {
       isPrefix,
       Utils
    }) => {
-      client.sendFile(m.chat, 'https://tinyurl.com/gdd01', '', m)
+      client.sendFile(m.chat, 'https://tinyurl.com/gdd01', m)
    },
    error: false
 }
