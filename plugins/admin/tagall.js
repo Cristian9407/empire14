77b44a55242f463f99d672ed8c31e275ceb7d52f
Cristian9407 <60494428+Cristian9407@users.noreply.github.com>
2025-12-19 22:39:52 -0500

    Update tagall.js

--- plugins/admin/tagall.js
+++ plugins/admin/tagall.js
@@ -12,8 +12,8 @@ export const run = {
       try {
          let member = participants.map(v => v.id)
          let readmore = String.fromCharCode(8206).repeat(4001)
-         let message = (!text) ? 'Hola a todos ' + await (await client.groupMetadata(m.chat)).subject : text
-         client.reply(m.chat, `乂  *E V E R Y O N E*\n\n*“${message}”*\n${readmore}\n${member.map(v => '◦  @' + v.replace(/@.+/, '')).join('\n')}`, m)
+         let message = (!text) ? 'Atención ' + await (await client.groupMetadata(m.chat)).subject : text
+         client.reply(m.chat, `乂  *T O D O S*\n\n*${message}*\n${readmore}\n${member.map(v => '◦  @' + v.replace(/@.+/, '')).join('\n')}`, m)
       } catch (e) {
          console.log(e)
          return client.reply(m.chat, Utils.jsonFormat(e), m)
