4008ee029ae82dc7a0ff08d861af98807a9328fb
Cristian9407 <60494428+Cristian9407@users.noreply.github.com>
2026-03-03 16:49:46 -0500

    Update pm2.config.cjs

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -2,7 +2,7 @@
 
 module.exports = {
    apps: [{
-      name: 'neoxr',
+      name: 'empire',
       script: './index.js',
       node_args: '--max-old-space-size=512', // Limit V8 heap to prevent JS memory leaks on a 1GB VPS
       max_memory_restart: '700M', // Restart app if real memory usage exceeds 700MB (leave room for OS)
