563132af5b7d72aa57ba8706896b7c2563b37142
Cristian9407 <60494428+Cristian9407@users.noreply.github.com>
2026-04-12 05:08:50 -0500

    Update filter.js

--- plugins/_events/protector/filter.js
+++ plugins/_events/protector/filter.js
@@ -1,4 +1,4 @@
-exports.run = {
+export const run = {
    async: async (m, {
       client,
       body,
@@ -7,7 +7,7 @@ exports.run = {
       setting,
       isAdmin,
       isBotAdmin,
-      Func
+      Utils
    }) => {
       try {
          if (groupSet.filter && !isAdmin && isBotAdmin && !m.fromMe) {
@@ -15,7 +15,7 @@ exports.run = {
             if (body && (new RegExp('\\b' + toxic.join('\\b|\\b') + '\\b')).test(body.toLowerCase())) {
                groupSet.member[m.sender].warning += 1
                let warning = groupSet.member[m.sender].warning
-               if (warning > 2) return client.reply(m.chat, Func.texted('bold', `🚩 Advertencia : [ 3 / 3 ], good bye ~~`), m).then(() => {
+               if (warning > 4) return client.reply(m.chat, Utils.texted('bold', `🚩 Warning : [ 5 / 5 ], good bye ~~`), m).then(() => {
                   client.groupParticipantsUpdate(m.chat, [m.sender], 'remove').then(async () => {
                      groupSet.member[m.sender].warning = 0
                      client.sendMessage(m.chat, {
@@ -28,7 +28,7 @@ exports.run = {
                      })
                   })
                })
-               return client.reply(m.chat, `乂  *W A R N I N G* \n\nRecibiste una advertencia : [ ${warning} / 3 ]\nSi recibes 3 advertencias serás expulsado automáticamente del grupo.`, m).then(() => client.sendMessage(m.chat, {
+               return client.reply(m.chat, `乂  *W A R N I N G* \n\nYou got warning : [ ${warning} / 5 ]\n\If you get 5 warnings you will be kicked automatically from the group.`, m).then(() => client.sendMessage(m.chat, {
                   delete: {
                      remoteJid: m.chat,
                      fromMe: isBotAdmin ? false : true,
@@ -39,9 +39,8 @@ exports.run = {
             }
          }
       } catch (e) {
-         return client.reply(m.chat, Func.jsonFormat(e), m)
+         return client.reply(m.chat, Utils.jsonFormat(e), m)
       }
    },
-   group: true,
-   exception: true
+   group: true
 }
