03c32acb9ab900379bd12a33e0a11f9037787fec
Cristian9407 <60494428+Cristian9407@users.noreply.github.com>
2026-07-06 13:00:45 -0500

    Update autodl_mediafire.js

--- plugins/_events/downloader/autodl_mediafire.js
+++ plugins/_events/downloader/autodl_mediafire.js
@@ -7,6 +7,7 @@ export const run = {
       body,
       users,
       Config,
+      setting,
       Utils
    }) => {
       try {
@@ -40,7 +41,11 @@ export const run = {
                   if (chSize.oversize) return client.reply(m.chat, isOver, m)
                   client.sendMessageModify(m.chat, text, m, {
                      largeThumb: true,
-                     thumbnail: 'https://telegra.ph/file/fcf56d646aa059af84126.jpg'
+                     type: 'preview-link',
+                     /* choose: landscape (default), potrait, square */
+                     ratio: 'landscape',
+                     thumbnail: 'https://telegra.ph/file/fcf56d646aa059af84126.jpg',
+                     icon: setting.icon ? Utils.isUrl(setting.icon) ? setting.icon : Buffer.from(setting.icon, 'base64') : null
                   }).then(async () => {
                      client.sendFile(m.chat, json.data.url, unescape(decode(json.data.title)), '', m)
                   })
